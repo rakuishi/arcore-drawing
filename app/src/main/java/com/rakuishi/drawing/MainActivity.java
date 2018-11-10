2318aea00f86c861477a3f12d618d0e4db744ec1
rakuishi <rakuishi@gmail.com>
2018-11-10 20:35:08 +0900

    Add ArFragment

--- app/src/main/java/com/rakuishi/drawing/MainActivity.java
+++ app/src/main/java/com/rakuishi/drawing/MainActivity.java
@@ -1,7 +1,9 @@
 package com.rakuishi.drawing;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.ar.sceneform.ux.ArFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArFragment arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
     }
 }
